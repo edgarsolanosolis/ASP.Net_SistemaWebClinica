73cb57e9a86fbe9e828f500d49f3f1306fed3c1b
edgarsolanosolis <bolivarsolano@hotmail.com>
2020-04-16 15:18:01 -0600

    Clases
    Se crean las clases y se agregan algunos contructores y atributos

--- SistemaClinica/CapaAccesoDatos/EmpleadoDAO.cs
+++ SistemaClinica/CapaAccesoDatos/EmpleadoDAO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaEntidades
+namespace CapaAccesoDatos
 {
-    public class Class1
+    class EmpleadoDAO
     {
     }
 }
--- SistemaClinica/CapaEntidades/Empleado.cs
+++ SistemaClinica/CapaEntidades/Empleado.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaAccesoDatos
+namespace CapaEntidades
 {
-    public class Class1
+    class Empleado
     {
     }
 }
